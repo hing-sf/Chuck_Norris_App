3dd6c1ba5c8c2115150707a478e82e699514b385
Hing Tang <hing.tang.sf@gmail.com>
2015-04-14 17:31:25 -0700

    More stuff

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express"),
     ejs = require("ejs"),
     bodyParser = require("body-parser"),
-   	methodOverride = require("method-override"),
-   	db = require("./models"),
-   	session = require("express-session"),
-   	request = require('request'),
-   	app = express();
+    methodOverride = require("method-override"),
+    db = require("./models"),
+    session = require("express-session"),
+    request = require('request'),
+    app = express();
 
 app.set('view engine', 'ejs');
 
@@ -15,9 +15,9 @@ app.use(methodOverride('_method'))
 
 // This defines req.session
 app.use(session({
-  secret: 'super secret',
-  resave: false,
-  saveUninitialized: true
+    secret: 'super secret',
+    resave: false,
+    saveUninitialized: true
 }))
 
 app.use(express.static('public'));
@@ -30,21 +30,21 @@ app.use("/", function (req, res, next) {
   };
 
   req.currentUser = function () {
-    return db.User.
-      find({
-        where: {
-          id: req.session.userId
-       }
-      }).
-      then(function (user) {
-        req.user = user;
-        return user;
-      })
+      return db.User.
+          find({
+              where: {
+                id: req.session.userId
+              }
+          }).
+          then(function (user) {
+              req.user = user;
+              return user;
+        })
   };
 
   req.logout = function () {
-    req.session.userId = null;
-    req.user = null;
+      req.session.userId = null;
+      req.user = null;
   }
 
   next(); 
@@ -52,133 +52,118 @@ app.use("/", function (req, res, next) {
 
 
 app.get('/', function(req,res){
-	res.render("index");
+    res.render("index");
 });
 
 
 //this render the signup page
 app.get('/signup', function(req,res){
-	res.render("user/signup");
+    res.render("user/signup");
 });
 
 // reference signup.ejs
 // this will post to the db after you click the signup button
 app.post('/signup', function(req,res){
-	var email = req.body.email;
-	var password = req.body.password;
-	db.User.createSecure(email,password)
-	  .then(function(user){
-	  	res.redirect('/profile');
-	  });
+    var email = req.body.email;
+    var password = req.body.password;
+    db.User.createSecure(email,password)
+        .then(function(user){
+          res.redirect('/profile');
+        });
 });
 
 
 // reference login.ejs
 // this is for the login to account
 app.get('/login', function(req,res){
-	req.currentUser().then(function(user){
-		if (user) { //if already logged in, will redirect to profile page
-			res.redirect('/profile');
-		} else { // if not logged in, you will be sent to login page
-			res.render("user/login");
-		}
-	});
+    req.currentUser().then(function(user){
+        if (user) { //if already logged in, will redirect to profile page
+            res.redirect('/profile');
+        } else { // if not logged in, you will be sent to login page
+            res.render("user/login");
+        }
+    });
 });
 
 // reference login.ejs
 // this is to authenticate the login
 app.post('/login', function(req,res){
-	var email = req.body.email;
-	var password = req.body.password;
-	db.User.authenticate(email,password)
-	  .then(function(dbUser){
-	  	if(dbUser) {
-	  		req.login(dbUser);
-	  		res.redirect('/profile');
-	  	} else {
-	  		res.redirect('/login');
-	  	}
-	  }); 
+  var email = req.body.email;
+  var password = req.body.password;
+  db.User.authenticate(email,password)
+      .then(function(dbUser){
+          if(dbUser) {
+              req.login(dbUser);
+              res.redirect('/profile');
+          } else {
+              res.redirect('/login');
+          }
+      }); 
 });
 
 // this route to the profile page
 app.get("/profile", function(req,res) {
-  req.currentUser()
-      .then(function(dbUser) {
-      	console.log("profile page works!!");
-        res.render('user/profile', {ejsUser: dbUser});
-      });
+    req.currentUser()
+        .then(function(dbUser) {
+            console.log("profile page works!!");
+            res.render('user/profile', {ejsUser: dbUser});
+        });
 });
 
 //this is to end the session
 app.delete('/logout', function(req,res){
-	req.logout();
-	res.redirect('/login');
+    req.logout();
+    res.redirect('/login');
 });
 
 
 // We have our search route that renders our search view
 app.get('/search', function(req,res) {
-	  var jokeSearch = req.query.js;
+    var category = req.query.category;
+    // var keywords = req.query.keywords;
+    // var numJokes = req.query.numJokes;
+    // var jr = req.query.jr;
+    // var category = [category,keywords,numJokes];
 
-    if(!jokeSearch) {
-  	   res.render("search", {jokes: [], noJokes: true});
-    } else {
-      var options = {
-        url: "https://webknox-jokes.p.mashape.com/jokes/search?category=" + jokeSearch,
-        headers: {
-          'X-Mashape-Key': 'TXdXq0nIjKmshMYUqcr6krjKGIIXp1I2RFyjsnU6yqrykgONUf'
-        }
-      };
+    if(!category) {
+        res.render("search", {jokes: [], noJokes: true});
+        console.log(category);
+        console.log("it's not working");
+        } else {
+        var options = {
+            url: "http://api.icndb.com/jokes/random/3",
+            // headers: {
+            //   'X-Mashape-Key': 'TXdXq0nIjKmshMYUqcr6krjKGIIXp1I2RFyjsnU6yqrykgONUf'
+          } 
+        };
 
-      request(options, function(err, response, body) {
-        if (!err && response.statusCode == 200) {
-          var jokes = JSON.parse(body); // this convert the JSON objects into something you can display
+        request(options, function(err, response, body) {
+          console.log(options);
+            if (!err && response.statusCode == 200) {
+                var jokes = JSON.parse(body); // this convert the JSON objects into something you can display
+                console.log(category);
+                console.log(jokes);
+                if (jokes) {
+                  res.render('search', { jokes: jokes, noJokes: false });
+                  console.log("With jokes");
+                } else {
+                  res.render('search', { jokes: [], noJokes: true });
+                  console.log("Without Jokes");
+                }
+            }
+        });
 
-          console.log(jokes);
-
-          if (jokes) {
-            res.render('search', { jokes: jokes, noJokes: false });
-          } else {
-            res.render('search', { jokes: [], noJokes: true });
-          }
-
-          // if (!jsonData.Search) {
-          //   res.render('search', {jokes: [], noJokes: true});
-          // }
-          
-          // res.render("search", {jokes: jsonData.Search, noJokes: false});
-        }
-      });
-
-    	// We use the request module to make a request to
-    	// jokes.p.mashape.com. We pass in a callback that takes in three
-    	// parameters, error, response, and body.
-  		// request(url, function(err,response,body) {
-    //     if (!err && response.statusCode == 200) {
-    //       console.log("THIS IS THE RESPONSE", response);
-    // 		  console.log(body); // Show the HTML for the jokes site. 
-
-    // 		  var jsonData = JSON.parse(body); // this convert the JSON objects into something you can display
-
-    //       if (!jsonData.Search) {
-    //         res.render('search', {jokes: [], noJokes: true});
-    //       }
-          
-    //       res.render("search", {jokes: jsonData.Search, noJokes: false});
-    //     }
-    //   });
-	  }
+    }
 });
 
 // We have our movie route that renders our movie view
 app.get('/joke', function(req,res) {
-  res.render('jokePage', {joke: {Category: "I'm the category", Joke: "I'm a joke"}});
+    res.render('jokePage', {joke: {Category: "I'm the category", Joke: "I'm a joke"}});
 });
 
 
 
 
 app.listen(3000, function () {
-  console.log("RUN SERVER RUN");
+    console.log("RUN SERVER RUN");
 });
